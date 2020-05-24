88544beb09cf68d55aa892cba5ebd89e8d51700e
NathanRV <49606381+NathanRV@users.noreply.github.com>
2020-05-24 16:07:59 -0400

    Commit propre sans fichier gradle (#36)
    * commit propre sans fichier gradle
    
    * Renommer input->reader et ouput->writer
    
    Co-authored-by: Aymeric <48229088+JustAymeric@users.noreply.github.com>
    
    * Reformat pour clarté code
    
    Co-authored-by: Aymeric <48229088+JustAymeric@users.noreply.github.com>
    
    * Changement de nom de variable (input,output)
    
    Reflecter le changement partout dans le code
    
    * Remplace loop infini par attente
    
    * Change paramètre de loop IP
    
    * Fonction non-statique
    
    * Thread non-statique
    
    * Methode non-statique
    
    * Correction statique
    
    * Lancer les threads à partir variable
    
    * InetAddress au lieu de String
    
    Co-authored-by: Aymeric <48229088+JustAymeric@users.noreply.github.com>
    
    * Ajouter Import de InetAddress
    
    * String -> InetAdress
    
    * Error handling InetAddress
    
    * reformat error print
    
    * code cleanup
    
    * InetAddress dans le main plutot que fct validation
    
    * Error handling InetAddress dans le main
    
    * Erreur frappe
    
    * Retour à string
    
    Vous implémenterez InetAddress dans vos futurs push et pull request si besoin est. Ce commit est simplement pour avoir un code qui fonctionne avec Gradle pour le moment.
    
    * Valid n'est jamais changé dans la boucle
    
    Co-authored-by: William Trépanier <39393794+wtrep@users.noreply.github.com>
    Co-authored-by: Aymeric <48229088+JustAymeric@users.noreply.github.com>

--- TP1/Client/src/main/java/client/Client.java
+++ TP1/Client/src/main/java/client/Client.java
@@ -1,24 +1,158 @@
 package client;
 
-import java.io.DataInputStream;
-import java.net.Socket;
+import java.net.*;
+import java.io.*;
 
 public class Client {
-    public static Socket socket;
+    private static Socket socket;
 
-    public static void main(String[] args) throws Exception {
-        String serverAddress = "127.0.0.1";
-        int port = 5000;
+    /*
+     * Application client
+     */
 
+    private static String validationIP(BufferedReader reader) {
+        //Demande IP
+        System.out.println("Veuillez entrez l'adresse IP du serveur : ");
+        //Initialisation de variable
+        String serverAddress = "";
+        String IP_PATTERN = "^((0|1\\d?\\d?|2[0-4]?\\d?|25[0-5]?|[3-9]\\d?)\\.){3}(0|1\\d?\\d?|2[0-4]?\\d?|25[0-5]?|[3-9]\\d?)$";
+        while (!serverAddress.matches(IP_PATTERN)) {
+            //Donnees entree par la console
+            try {
+                serverAddress = reader.readLine();
+            }
+            catch(IOException e) {
+                e.printStackTrace();
+            }
+            if(!serverAddress.matches(IP_PATTERN))
+                System.out.println("Adresse IP entrée invalide! Veuillez entre une adresse du format XXX.XXX.XXX.XXX : ");
+        }
+        
+        return serverAddress;
+    }
+
+    private static int validationPort(BufferedReader reader) {
+        //Demande Port
+        System.out.println("Veuillez entrez le port d'écoute du serveur : ");
+        int serverPort = 0;
+        while( serverPort<5000 || serverPort>5050 )
+        {
+            //Donnees entrées par la console
+            try {
+                serverPort = Integer.parseInt(reader.readLine());
+            }
+            catch(Exception e) {
+                serverPort = 0;
+            }
+            if (!(serverPort<5000 || serverPort>5050))
+                System.out.println("Port invalide! (Veuillez entrez un nombre entre 5000 et 5050) ");
+        }
+
+        return serverPort;
+    }
+
+    private static class SendMessage extends Thread {
+        BufferedReader userIn; 
+        BufferedWriter writer;  
+        boolean connected;
+
+        public SendMessage(Socket socket) {
+            userIn = new BufferedReader(new InputStreamReader(System.in));
+            connected = true;
+            try {
+                writer = new BufferedWriter(new PrintWriter(socket.getOutputStream()));
+            } catch (IOException e) {
+                System.out.println("Le socket n'est pas connecté veuillez réessayer.");
+                connected = false;
+            }
+        }
+
+        public void run() {
+            String message;
+            String inputMessage;
+            while(true) {
+                try {
+                    //Chercher input
+                    inputMessage = userIn.readLine();
+
+                    //TODO Former message
+                    message = inputMessage;
+
+                    //Envoyer le message
+                    writer.write(message);
+                    writer.newLine();
+                    writer.flush();
+                }
+                catch(IOException e) {
+                    System.out.println("Erreur dans l'envoi du message! Déconnexion.");
+                    connected = false;
+                }
+            }
+        }
+    }
+
+    private static class ReadMessage extends Thread {
+        BufferedReader receiver;
+        boolean connected;
+
+        public ReadMessage(Socket socket) {
+            connected = true;
+            try {
+                receiver = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            } catch (IOException e) {
+                System.out.println("Le socket n'est pas connecté veuillez réessayer.");
+                connected = false;
+            }
+        }
+
+        public void run() {
+            String message;
+            while(connected) {
+                try {
+                    //Chercher le message
+                    message = receiver.readLine();
+
+                    //Afficher le message
+                    System.out.println(message);
+                }
+                catch(IOException e) {
+                    System.out.println("Erreur dans la réception du message! Déconnexion.");
+                    connected = false;
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception
+    {
+        //Issue #11 : Saisie des paramètres du serveur (adresse IP, port d'écoute entre 5000 et 5050)
+        //Canal d'entree de la console
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        //Addresse et port du serveur
+        String serverAddress = validationIP(reader);
+        int port= validationPort(reader);
+
+        //TEST pour receiveMessage(socket)
+        //Creation d'une nouvelle connexion avec le serveur
         socket = new Socket(serverAddress, port);
+//		BufferedReader serverReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        try {
+            //Écriture, reception
+            SendMessage send = new SendMessage(socket);
+            send.start();
+            ReadMessage read = new ReadMessage(socket);
+            read.start();
+            //Attendre que les threads finissent
+            send.join();
+            read.join();
+        }
+        finally {
+            System.out.println("Tu as été déconnecté du serveur!");
 
-        System.out.format("The server is running on %s:%d%n", serverAddress, port);
-
-        DataInputStream in = new DataInputStream(socket.getInputStream());
-
-        String helloWorldFromServer = in.readUTF();
-        System.out.println(helloWorldFromServer);
-
-        socket.close();
+            //Fermeture de la connexion avec le serveur
+            socket.close();
+        }
     }
 }
